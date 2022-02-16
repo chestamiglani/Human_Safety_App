192e56c0ddd38adf7bad8d50f9660fa9effb99da
chestamiglani <chestamiglani18@gmail.com>
2022-02-16 16:32:28 +0530

    Final Commit

---
+++ app/src/main/java/com/sos/humansafetyapp/splash.java
@@ -0,0 +1,24 @@
+package com.sos.humansafetyapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            Intent intent=new Intent(splash.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
